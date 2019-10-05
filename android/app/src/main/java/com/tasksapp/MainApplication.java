0736c8ce3799896606f73be0e6cc1f6a0b6eed1a
ahmedsalihh <ahmedsalihh@gmail.com>
2019-10-06 00:07:01 +0300

    Add firebase to login scrren navigate signup if non user

--- android/app/src/main/java/com/tasksapp/MainApplication.java
+++ android/app/src/main/java/com/tasksapp/MainApplication.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -26,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+            packages.add(new RNFirebaseAuthPackage());
+            packages.add(new RNFirebaseFirestorePackage());
           return packages;
         }
 
