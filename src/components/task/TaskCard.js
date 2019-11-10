b9bec1d3e3a8089b2fb60c58cb54a2effeb408ee
ahmedsalihh <ahmedsalihh@gmail.com>
2019-11-10 03:18:11 +0300

    add good checkbox to tasks

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -1,71 +1,27 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  // CheckBox,
-} from 'react-native';
-import CheckBox from '@react-native-community/checkbox';
-// import {CheckBox} from 'react-native-elements';
+import {View, SafeAreaView} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 const ListItem = props => {
   return (
-    <View style={styles.container}>
-      <CheckBox
-        value={props.task.task_status}
-        onValueChange={() =>
-          props.setChecked({
-            task: {...props.task, task_status: !props.task.task_status},
-          })
-        }
-      />
-      <Text>{props.task.name}</Text>
+    <View style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{margin: 16}}>
+          <BouncyCheckbox
+            onPress={() =>
+              props.setChecked({
+                task: {...props.task, task_status: !props.task.task_status},
+              })
+            }
+            borderColor="#cf1748"
+            fillColor="#cf1748"
+            isChecked={props.task.task_status}
+            text={props.task.name}
+          />
+        </View>
+      </SafeAreaView>
     </View>
-    // <View style={styles.container}>
-    //   <View style={styles.textContainer}>
-    //     <Text style={styles.taskName}>{props.task.name}</Text>
-    //   </View>
-    //   <View style={styles.imageContainer}>
-    //     <TouchableOpacity activeOpacity={0.5} onPress={handleDelete}>
-    //       <Image
-    //         source={require('../../assets/images/android/outline_delete_white_24.png')}
-    //         style={styles.imageIconStyle}
-    //       />
-    //     </TouchableOpacity>
-    //   </View>
-    // </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-  },
-  textContainer: {
-    flex: 1,
-    alignItems: 'flex-start',
-  },
-  imageContainer: {
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  taskName: {
-    color: '#000',
-    fontSize: 20,
-    textAlign: 'left',
-    paddingLeft: '5%',
-  },
-  imageIconStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: 'stretch',
-  },
-});
 
 export default ListItem;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import {createStore, applyMiddleware, compose} from 'redux';
+import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const configureStore = preloadedState => {
   const middlewares = [thunk];
+  if (process.env.NODE_ENV === 'development') {
+    middlewares.push(createLogger());
+  }
   const composed = [applyMiddleware(...middlewares)];
 
   return createStore(rootReducer, preloadedState, compose(...composed));
